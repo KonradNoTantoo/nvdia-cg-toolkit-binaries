24087d20d6be0c85f461fbb15ba6392a6875bc2d
Knife <konrad@no.tantoo>
2020-03-06 22:14:37 +0100

    Enforce docker32 images for x86

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(docker_32_images=True)
     builder.add_common_builds()
     builder.run()
